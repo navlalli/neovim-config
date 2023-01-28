750b2d2e13d76c70191541b000a778f0e03269d3
Navraj Lalli <navrajsinghlalli@gmail.com>
2023-01-28 20:35:23 +0000

    Keybindings for searching commonly used dirs

--- lua/telescope/teleconfig.lua
+++ lua/telescope/teleconfig.lua
@@ -7,5 +7,8 @@ require('telescope').load_extension('fzf')
 -- Keymaps
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>hh', ":Telescope find_files cwd=~/navScripts/ <CR>", {})  -- Also works
+vim.keymap.set('n', '<leader>fn', "<cmd>Telescope find_files cwd=~/navScripts/ <CR>", {}) -- FindNav
+vim.keymap.set('n', '<leader>fh', "<cmd>Telescope find_files cwd=~/OneDrive/PhD/howTo/ <CR>", {}) -- FindHome
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
