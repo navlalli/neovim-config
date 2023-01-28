adc67c23723948597725ca9f38af4d7209ee3a88
Navraj Lalli <navrajsinghlalli@gmail.com>
2023-01-28 20:00:12 +0000

    Ideas for easier installation of language servers - mason.nvim

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -1,4 +1,8 @@
--- lsp configuration 
+-- lsp configuration - consider switching to lsp-zero which uses mason to allow
+-- for the ensure_installed section. This is good for quickly setting up a new 
+-- language and to avoid extra setup on a new machine.
+-- https://github.com/ThePrimeagen/init.lua/
+-- Checkout https://github.com/nvim-lua/kickstart.nvim 
 -- bash
 require'lspconfig'.bashls.setup{
     on_attach = function()
@@ -7,6 +11,7 @@ require'lspconfig'.bashls.setup{
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {buffer=0})
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {buffer=0})
     end,  
 }
 
