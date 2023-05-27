ea0a58b67028377884e3091c16e68701be4432c7
Navraj Lalli <navrajsinghlalli@gmail.com>
2023-05-27 18:11:19 +0100

    Rewrite find_files key binding

--- lua/telescope/teleconfig.lua
+++ lua/telescope/teleconfig.lua
@@ -6,7 +6,7 @@ require('telescope').load_extension('fzf')
 
 -- Keymaps
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files <CR>", {}) -- FindFiles (current directory)
 -- vim.keymap.set('n', '<leader>hh', ":Telescope find_files cwd=~/navScripts/ <CR>", {})  -- Also works
 vim.keymap.set('n', '<leader>fn', "<cmd>Telescope find_files cwd=~/navScripts/ <CR>", {}) -- FindNav
 vim.keymap.set('n', '<leader>fh', "<cmd>Telescope find_files cwd=~/OneDrive/PhD/howTo/ <CR>", {}) -- FindHow
